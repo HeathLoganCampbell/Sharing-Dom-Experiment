63fdeda5ce297cf8ffbe98858774a3768d550f5c
HeathLoganCampbell <GitHub@HeathLoganCampbell.com>
2019-05-28 14:56:40 +1200

    init of stylesheet downloader

--- server.js
+++ server.js
@@ -2,10 +2,13 @@
 //mirror["applyChanged"].apply(mirror, [{id: 28}], [{id: 34, nodeType: 3, parentNode: {id: 27}, previousSibling: null, textContent: "Heath - mm)"}], [], []);
 const express = require("express");
 var app = express();
-var http = require("http").Server(app);
-var io = require("socket.io")(http);
+const http = require("http");
+var https = require("https");
+var httpServer = http.Server(app);
+var io = require("socket.io")(httpServer);
 const JSON = require("circular-json");
 var bodyParser = require("body-parser");
+const fs = require("fs");
 
 app.use(express.static("./"));
 
@@ -31,11 +34,36 @@ app.get("/collect/assets", function(req, res) {
 });
 //END COLLECTORS
 
+//START PROXY
+// All the resources we download will be put here, so we have an easy access point to get them
+//proxy/(UUID OF SITE)/(URL)
+//should date be included?
+app.get("/proxy", function(req, res) {
+	res.sendFile(__dirname + "/player.html");
+});
+//END PROXY
+
 changeHistory = [];
 io.on("connection", function(socket) {
 	socket.on("recorder", function(msg) {
 		console.log(msg);
 		changeHistory.push(msg);
+		if (msg.type == "stylesheets") {
+			console.log("sheets!!!!");
+			for (var i = 0; i < msg.args[0].length; i++) {
+				console.log("Downloading");
+				const file = fs.createWriteStream("copy.css");
+				const url = msg.args[0][i];
+				if (url.includes("https"))
+					https.get(url, function(response) {
+						response.pipe(file);
+					});
+				else
+					http.get(url, function(response) {
+						response.pipe(file);
+					});
+			}
+		}
 		socket.broadcast.emit("player", msg);
 	});
 
@@ -45,6 +73,6 @@ io.on("connection", function(socket) {
 	});
 });
 
-http.listen(3000, function() {
+httpServer.listen(3000, function() {
 	console.log("listening on *:3000");
 });
